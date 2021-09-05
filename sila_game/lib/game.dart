463658ac4285852add1185a84cca1e8f565de767
MijaelW@tts <>
2021-09-05 00:15:17 -0500

    Images Appear Based on Syllable
    - If syllable is `ba` the img `ballena` is displayed, and so on.
    - For now the `Random` of the constants and many constants were removed for testing purposes.
    - A fixed height was set on all the images

--- sila_game/lib/game.dart
+++ sila_game/lib/game.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -10,65 +8,35 @@ class Game extends StatefulWidget {
 
 class _GameState extends State<Game> {
   static const String TITLE = 'Sila Game';
-  static const String PLACEHOLDER_IMAGE = 'assets/images/animals/le1.jpg';
-
-  static const List<String> VOWELS = ['A', 'E', 'I', 'O', 'U'];
-  static const List<String> CONSONANTS = [
-    'B',
-    'C',
-    'D',
-    'F',
-    'G',
-    'H',
-    'J',
-    'K',
-    'L',
-    'M',
-    'N',
-    'Ñ',
-    'P',
-    'R',
-    'S',
-    'T',
-    'V',
-    'W',
-    'X',
-    'Y',
-    'Z'
-  ];
-  static const List<String> IMAGES = [
-    'assets/images/animals/ca.png',
-    'assets/images/animals/ce.jpg',
-    'assets/images/animals/ga.jpg',
-    'assets/images/animals/le1.jpg',
-    'assets/images/animals/le2.png',
-    'assets/images/animals/ov.png',
-    'assets/images/animals/pa.png',
-    'assets/images/animals/po.png',
-    'assets/images/animals/ti.png',
-    'assets/images/animals/va.png',
-    'assets/images/animals/ze.jpg',
-  ];
+  static const String IMAGE_PATH = 'assets/images/';
+  static const List<String> CONSONANTS = ['b', 'c'];
+  static const List<String> VOWELS = ['a', 'e', 'i', 'o', 'u'];
 
   int _vowelNumber = 0;
-  int _consonantNumber = Random().nextInt(20);
+  int _consonantNumber = 0; // Random().nextInt(1);
 
-  /// The idea for later is to do it randomly or in order
+  /// The idea for later is to do it randomly and in order
   void _updateSila() {
     setState(() {
       if (_vowelNumber < VOWELS.length - 1) {
         _vowelNumber++;
       } else {
         _vowelNumber = 0;
-        _consonantNumber = Random().nextInt(20);
+        // Random will be inserted later again.
+        _consonantNumber = _consonantNumber == 0 ? 1 : 0;
       }
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    // TODO: should I move these two later on?
     String syllable =
         CONSONANTS[_consonantNumber] + '  ' + VOWELS[_vowelNumber];
+    String syllableImagePath = IMAGE_PATH +
+        CONSONANTS[_consonantNumber] +
+        VOWELS[_vowelNumber] +
+        '.jpg';
 
     /// Image pressing = animal name with voice + animation in image
     return Scaffold(
@@ -88,7 +56,7 @@ class _GameState extends State<Game> {
                   height: 90,
                   child: Center(
                     child: Text(
-                      syllable,
+                      syllable.toUpperCase(),
                       style:
                           TextStyle(fontWeight: FontWeight.bold, fontSize: 80),
                     ),
@@ -96,7 +64,10 @@ class _GameState extends State<Game> {
                 ),
               ),
             ),
-            Image(image: AssetImage(IMAGES[_vowelNumber])),
+            Image(
+              image: AssetImage(syllableImagePath),
+              height: 250,
+            ),
           ],
         ),
       ),
